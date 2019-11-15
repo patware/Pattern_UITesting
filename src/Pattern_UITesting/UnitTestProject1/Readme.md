283dc9719a96e7e22048d1610361990be96ba759
Calvé, Patrice <patware@hotmail.com>
2019-11-14 21:29:19 -0500

    The UnitTest1 TestMethod1 now: hosts a Mockup version of the Ping Service, launches the WpfClient via Appium which uses WinAppDriver, hits the Ping Button that calls back the Unit Test's Ping Mockup.

---
+++ src/Pattern_UITesting/UnitTestProject1/Readme.md
@@ -0,0 +1,16 @@
+﻿# Setting up this UI Test
+
+This UnitTest Project is actually a UI Test.  Some setup is required.
+
+## WinAppDriver
+
+The UnitTest1 uses Appium to call WinAppDriver that takes care of launching the WpfClient and finding its UI Controls.
+
+* Appium: installed using the Nuget package: Appium.WebDriver
+* WinAppDriver: separate install: https://github.com/microsoft/WinAppDriver
+	* Download the latest (I used the .msi)
+	* Run the .msi installer
+	* Launch the WinAppDriver.  On my machine it was installed on "C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe"
+	* Note the port.  It should indicate something like 4723.  You'll need it in the launch options, UnitTest1 line 45
+
+Note that the WinAppDriver docs on github have obsolete samples.  https://github.com/microsoft/WinAppDriver/blob/master/Docs/AuthoringTestScripts.md
--- src/Pattern_UITesting/UnitTestProject1/UnitTest1.cs
+++ src/Pattern_UITesting/UnitTestProject1/UnitTest1.cs
@@ -12,6 +12,7 @@ namespace UnitTestProject1
         [TestInitialize]
         public void Init()
         {
+            System.Diagnostics.Debug.WriteLine($"Starting the PingMock service at Port {Port}");
             PingServiceMock = new Mocking.PingMock();
             
             Server = new Grpc.Core.Server
@@ -21,16 +22,35 @@ namespace UnitTestProject1
             };
 
             Server.Start();
+            System.Diagnostics.Debug.WriteLine($"Server Started.");
         }
         [TestCleanup]
         public void Cleanup()
         {
+            System.Diagnostics.Debug.WriteLine($"Stopping Service...");
             Server.ShutdownAsync().Wait();
+            System.Diagnostics.Debug.WriteLine($"We're done.");
         }
         [TestMethod]
         public void TestMethod1()
         {
-            PingServiceMock.PingReturn = "Pong";
+            System.Diagnostics.Debug.WriteLine($"TestMethod1: Setting up Appium");
+            PingServiceMock.PingReturn = "Pong From Mock";
+
+            var options = new OpenQA.Selenium.Appium.AppiumOptions();
+            var exeLocation = System.IO.Path.GetFullPath(@"..\..\..\..\WpfApp1\bin\Debug\netcoreapp3.0\WpfApp1.exe");
+            options.AddAdditionalCapability("app",exeLocation);
+            
+            var wpfSession = new OpenQA.Selenium.Appium.Windows.WindowsDriver<OpenQA.Selenium.Appium.Windows.WindowsElement>(
+                new System.Uri("http://127.0.0.1:4723"),
+                options, 
+                new System.TimeSpan(1,0,0));
+
+            var pingButton = wpfSession.FindElementByAccessibilityId("PingButton");
+            pingButton.Click();
+
+            var pingLabel = wpfSession.FindElementByAccessibilityId("PingLabel");
+            Assert.AreEqual("Pong From Mock", pingLabel.Text);
 
         }
     }
